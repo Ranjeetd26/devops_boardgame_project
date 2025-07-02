87f16f5f8c96bd896e5a0db4645991a120b6ab5b
Ranjeetd26 <ranjeetd.1999@gmail.com>
2025-07-02 08:17:03 +0530

    serverscript update with prject name

--- serverscript.sh
+++ serverscript.sh
@@ -3,15 +3,15 @@ sudo yum install git -y
 sudo yum install docker -y
 sudo systemctl start docker
 
-if [ -d "boardgame" ]
+if [ -d "devops_boardgame_project" ]
 then
   echo "repo is cloned and exists"
-  cd /home/ec2-user/boardgame
+  cd /home/ec2-user/devops_boardgame_project
   git pull origin master
 else
   git clone https://github.com/Abhi1279/devops_boardgame_project.git
 fi
 
-cd /home/ec2-user/boardgame
+cd /home/ec2-user/devops_boardgame_project
 git checkout master
 sudo docker build -t $1
